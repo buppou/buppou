407d2a63aad774df6bba9c3279ed100787b8784c
pasestars <pasestars@gmail.com>
2025-05-10 13:47:40 +0200

    feat: add module tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <div>Hello World</div>
+    <div class="text-red-900">Hello World</div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   vite: {
     server: {
       allowedHosts: ["3000.coder.buppou.com"]
     }
-  }
+  },
+
+  modules: ["@nuxtjs/tailwindcss"]
 })
